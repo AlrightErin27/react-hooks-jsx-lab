0d3f6d6c397b4405dbdc01a675c74c8b30a5d251
Erin Van Brunt <erinmontybruce@gmail.com>
2022-03-16 16:05:49 -0700

    done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h1 style={{ backgroundColor: "firebrick" }}>About</h1>
+      <h2>About Me</h2>
+      <p>
+        <img src={image} alt="I made this" />
+      </p>
+    </div>
+  );
 }
 
 export default About;
